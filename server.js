5441135fd2d49d3ca0cd0d3704e2257cd985d0df
James Howard <jth2698@gmail.com>
2020-12-14 21:43:15 -0600

    update server.js for new routes and mongodb

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+const routes = require("./routes");
+
+const mongoose = require("mongoose");
+
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -12,13 +16,14 @@ if (process.env.NODE_ENV === "production") {
 }
 
 // Define API routes here
+app.use(routes);
 
-// Send every other request to the React app
-// Define any API routes before this runs
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
+// Connect to the Mongo DB
+mongoose.connect(
+    process.env.MONGODB_URI || "mongodb://localhost/reactgooglebooks"
+);
 
+// Start the API server
 app.listen(PORT, () => {
     console.log(`🌎 ==> API server now on port ${PORT}!`);
 });
